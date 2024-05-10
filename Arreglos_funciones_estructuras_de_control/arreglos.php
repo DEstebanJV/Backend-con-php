d775e91640987d3ec5a4db534231640709a7dbb3
DestebanJV <dejimenez04@misena.edu.co>
2024-05-09 19:13:54 -0500

    Feat: update array asociative

--- Arreglos_funciones_estructuras_de_control/arreglos.php
+++ Arreglos_funciones_estructuras_de_control/arreglos.php
@@ -8,3 +8,19 @@
     echo "una de las edades es: " . $edades[1];
 
 ?>
+
+<h2>Arreglos asociativos:</h2>
+<p>una variable que puede guardar multiples valores y puedes accederr a ellos mediante una palabra </p>
+
+<?php 
+
+    $edades = [
+        "Juan" => 20,
+        "pedro" => 10,
+        "Alejandro" => 15,
+    ];
+
+    echo "la edad de Juan es " . $edades['Juan'] ;
+
+?>
+
