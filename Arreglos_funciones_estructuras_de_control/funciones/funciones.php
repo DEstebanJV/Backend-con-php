72c28a23976cb3008ab138cffe1ae8f89568f05c
DestebanJV <dejimenez04@misena.edu.co>
2024-05-14 18:46:11 -0500

    Feat: format document

--- Arreglos_funciones_estructuras_de_control/funciones/funciones.php
+++ Arreglos_funciones_estructuras_de_control/funciones/funciones.php
@@ -1,61 +1,60 @@
 <h1>Aprende a reutilizar tu codigo</h1>
 <p>Por medio de las funciones podemos darle una reutilizaciona nuestro codigo</p>
 <b>¿Que es una funcion?</b>
-<p>Podemos hacer la analogica: 
+<p>Podemos hacer la analogica:
     Podemos verlas como un chef magico al cual tu le das los ingredientes y te devuelve un pastel ya hecho
 </p>
 <p>En resumen</p>
 <p>una funcion nos sirve para separar todo un bloque de codigo ponerle
-     un nombre y utilizarla cuantas veces queramos sin preocuptarnos por como trabaja por dentro</p>
+    un nombre y utilizarla cuantas veces queramos sin preocuptarnos por como trabaja por dentro</p>
 
-
-     
 <h3>
-    Uso del return en las funciones 
+    Uso del return en las funciones
 </h3>
 <p>Retornar un valor significa decilr explicitamente a la funcion que es lo que debe devolver</p>
 <p>Esto lo hacemos con la palabra reservada "Return" y usualmente guardamos ese valor en una variable</p>
 
- <?php 
+<?php
 # Nota Los valores enviado en los parentesis de la funcion, Son llamados parametros
 
- function  getPokemon(int $pokeElegido) : string {
+function  getPokemon(int $pokeElegido): string
+{
     $pokemon = array("Pikachu", "Charmander", "Squirtle", "Bulbasaur");
 
-    switch($pokeElegido){
+    switch ($pokeElegido) {
         case 0:
             return $pokemon[0];
-        break;    
+            break;
         case 1:
             return $pokemon[1];
-        break;   
+            break;
         case 2:
             return $pokemon[2];
-        break; 
+            break;
         case 3:
             return $pokemon[3];
-        break;
+            break;
         default:
-        return "No es un pokemon de la lista";
+            return "No es un pokemon de la lista";
     }
- }  
+}
 
- getPokemon(rand(0,3)); // para mostrar agrega el echo 
- 
- ?>    
+getPokemon(rand(0, 3)); // para mostrar agrega el echo 
+
+?>
 
 
 <h2>Profundizando en parametros</h2>
 
-<?php 
+<?php
 // adicionando valores por defecto
-function suma($a = 0, $b = 0){
+function suma($a = 0, $b = 0)
+{
 
-    return "la suma de $a + $b es ". $a + $b . "<br>";
-
+    return "la suma de $a + $b es " . $a + $b . "<br>";
 }
 
-suma(7,89);
+suma(7, 89);
 
 # valores por defecto
 suma();
@@ -66,8 +65,8 @@ suma(9);
 
 #juntar arreglos
 
-$arreglo1 = [1,2,3];
-$arreglo2 = [4,5,6];
+$arreglo1 = [1, 2, 3];
+$arreglo2 = [4, 5, 6];
 
 
 $resultado = [...$arreglo1, ...$arreglo2];
@@ -76,20 +75,21 @@ $resultado = [...$arreglo1, ...$arreglo2];
 
 
 #Aplicado a funciones 
-$numeros = [1,2,3];
+$numeros = [1, 2, 3];
 
-function sumaArreglos($a, $b){
-    return "la suma de $a + $b es ". $a + $b . "<br>";
+function sumaArreglos($a, $b)
+{
+    return "la suma de $a + $b es " . $a + $b . "<br>";
 }
 
 suma(...$numeros);
 
 #forma inversa parametros de forma dinamica
 
-function sumaInfinita(...$params){
-  
-    return "la suma de los numeros es ". array_sum($params) . "<br>";
-}
+function sumaInfinita(...$params)
+{
 
-echo sumaInfinita(1,2,1,1);
+    return "la suma de los numeros es " . array_sum($params) . "<br>";
+}
+sumaInfinita(1, 2, 1, 1); //adicionar echo para probar
 ?>
