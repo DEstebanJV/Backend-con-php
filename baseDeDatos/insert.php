eba7e7ab7d85f95cc564f195967415b75dd631e8
DestebanJV <dejimenez04@misena.edu.co>
2024-05-19 14:47:57 -0500

    Feat: update file insert

--- baseDeDatos/insert.php
+++ baseDeDatos/insert.php
@@ -1,11 +1,12 @@
 <?php 
 
 
+require_once 'conexion.php';
 
 function create_user(string $cc, string $nombre, string $apellido, string $correo, $fecha ){
+  global $mysqli;
     $sql = "INSERT INTO tbl_usuario (cc, nombre, apellido, correo, fecha_nac)
     VALUES ('$cc', '$nombre', '$apellido', '$correo', '$fecha') ";
-require_once 'conexion.php';
 if ($mysqli->query($sql) === TRUE) {
     echo "New record created successfully";
   } else {
